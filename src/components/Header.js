114ac62478188d7952bbce777c942704b767d0f1
Xavyr Moss <xmoss10@gmail.com>
2018-03-07 20:35:00 -0800

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Header extends Component {
 	render() {
 		return ( 
-		<div className="header">
+		<div className="theHeader">
 			<h1>Rest In Peace Rest API's...It's GraphQL's time to Crud</h1>
 		</div>
 		);
